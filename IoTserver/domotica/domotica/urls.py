2dd4c00c137766189021cfeee5daf103024487ca
lobinhojr <renatolobjr@gmail.com>
2020-03-20 12:52:21 -0300

    página simples pronta

--- IoTserver/domotica/domotica/urls.py
+++ IoTserver/domotica/domotica/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include, url
+
 from django.urls import path
+from domotica.core import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    url(r'admin/', admin.site.urls),
 ]
