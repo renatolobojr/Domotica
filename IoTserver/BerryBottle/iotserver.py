97232f3cab03201fb563465d948554a7e2923a98
lobinhojr <renatolobjr@gmail.com>
2020-03-23 17:10:29 -0300

    pequenos reparos no frontend

--- IoTserver/BerryBottle/iotserver.py
+++ IoTserver/BerryBottle/iotserver.py
@@ -1,4 +1,7 @@
+#import raspi
 from bottle import run, route, template, request, static_file
+
+#raspi = raspi.Raspi() #instaciando um objeto da classe raspi.
 
 @route('/')
 def index():
@@ -23,5 +26,12 @@ def login():
         </form>
     '''
 
+#  value = raspi.read_sensor()
+#
+#    raspi.change_led(False)
+#
+# https://dreamingecho.es/blog/internet-of-things-with-python-and-flask
+# https://github.com/renatolobojr/flask-internet-of-things-app/tree/master/templates
 
-run(host='0.0.0.0', port=8080, debug=True)
+run(host='0.0.0.0', port=8080, debug=True) #
+
